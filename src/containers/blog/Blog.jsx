a670426598ae3e5983c6ecc5e03dce83ad374944
praveen-zag <praveensingh190720@gmail.com>
2023-09-02 10:26:38 +0530

    Initial commit

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import{ blog01, blog02, blog03, blog04, blog05 } from './imports';
+
+import Article from '../../components/article/Article'
+import './blog.css';
+const Blog = () => {
+  return (
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className='gradient__text'>
+        A lot is hapepning,
+        We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className='gpt3__blog-container_grpA'>
+          <Article imgURL={blog01} date="Sep 26, 2023" title={"GPT-3 and Open  AI is the future. Let us exlore how it is?"}/>
+        </div>
+        <div className='gpt3__blog-container_grpB'>
+          <Article imgURL={blog02} date="Sep 26, 2023" title={"GPT-3 and Open  AI is the future. Let us exlore how it is?"}/>
+          <Article imgURL={blog03} date="Sep 26, 2023" title={"GPT-3 and Open  AI is the future. Let us exlore how it is?"}/>
+          <Article imgURL={blog04} date="Sep 26, 2023" title={"GPT-3 and Open  AI is the future. Let us exlore how it is?"}/>
+          <Article imgURL={blog05} date="Sep 26, 2023" title={"GPT-3 and Open  AI is the future. Let us exlore how it is?"}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
